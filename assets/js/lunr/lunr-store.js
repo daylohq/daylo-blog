0598153a2800ee8d043327c91d04cb3a487b215a
Val Kantchev <vkantchev@users.noreply.github.com>
2020-10-09 14:45:10 -0700

    Auto push at 2020-10-09 14:45:10

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,106 @@
+var store = [{
+        "title": "First Release",
+        "excerpt":"First Daylo Meet version is now officially out in the wild.   If you are looking for a simple app for 1-on-1 online video meetings give it a try.   We think you will like it.   ","categories": ["blog"],
+        "tags": ["news"],
+        "url": "https://blog.daylo.com/first-release/",
+        "teaser":null},{
+        "title": "Vidello Glass",
+        "excerpt":"A drawing is worth a thousand words At those times when words are not enough, you can now open a board and quickly sketch out your ideas for everybody to see, and not only that, but they can draw with you on the same board too. Yep. We are proud...","categories": ["blog"],
+        "tags": ["news"],
+        "url": "https://blog.daylo.com/vidello-glass/",
+        "teaser":null},{
+        "title": "Distraction Free Mode",
+        "excerpt":"What is distraction free mode? In distraction free mode your browser switches to full screen and replaces your selfie with the silhouette of your head. Because you cannot see other windows or yourself you can give your full attention to your party and the conversation you are having. This is...","categories": ["blog"],
+        "tags": ["news"],
+        "url": "https://blog.daylo.com/distraction-free-mode/",
+        "teaser":null},{
+        "title": "Firefox",
+        "excerpt":"Rejoice you millions of Firefox fans! We worked hard for several months to give you Daylo Meet. So that now you can see each other in high definition during video calls. No plugins or extensions required. Here is how to use it: Go to https://meet.daylo.com: Allow Daylo Meet to access...","categories": ["blog"],
+        "tags": ["news","firefox"],
+        "url": "https://blog.daylo.com/firefox/",
+        "teaser":null},{
+        "title": "Safari",
+        "excerpt":"Once upon a time there was Safari 10, and it met Vidello and said “How can I help?”. And Vidello said “I like you a lot but unfortunately you do not have real-time video. I am sorry but I cannot work with you at this time.” Months passed, and more...","categories": ["blog"],
+        "tags": ["news","safari"],
+        "url": "https://blog.daylo.com/safari/",
+        "teaser":null},{
+        "title": "Improved Distraction Free Mode",
+        "excerpt":"We introduced Distraction Free Mode back in July and at the time we were pretty happy with how we designed it. However over time it became clear that the DFM notification popup window was probably not a good idea. So we had to do something about it and here is...","categories": ["blog"],
+        "tags": ["news"],
+        "url": "https://blog.daylo.com/improved-distraction-free-mode/",
+        "teaser":null},{
+        "title": "Edge",
+        "excerpt":"We are proud to announce that Daylo Meet video calls now work on latest Microsoft Edge … and that means that you can now use Vidello with the four major browsers - Chrome, Edge, Firefox, and Safari. Enjoy! NOTE: Microsoft does not offer separate updates to their browser app so...","categories": ["blog"],
+        "tags": ["news","edge"],
+        "url": "https://blog.daylo.com/edge/",
+        "teaser":null},{
+        "title": "Daylo",
+        "excerpt":"Vidello Inc. is now Daylo Inc.   We are building the next generation of communication and collaboration tools for the distributed workforce of the future.   So we changed our name to better reflect our broader goals and mission.     ","categories": ["blog"],
+        "tags": ["news","daylo"],
+        "url": "https://blog.daylo.com/daylo/",
+        "teaser":null},{
+        "title": "Keep Yourself on Track (and Sane) When Working From Home",
+        "excerpt":"You have this project to finish, but the garage needs to be cleaned. Better take the Christmas lights down, oh, and make sure you save some time for checking out and replying to LinkedIn and Twitter posts. If this sounds familiar to you, it’s probably because you’re trying to keep...","categories": ["blog"],
+        "tags": ["remote-work","productivity"],
+        "url": "https://blog.daylo.com/working-from-home/",
+        "teaser":"https://blog.daylo.com/assets/images/working-from-home/working-from-home.png"},{
+        "title": "Screen Sharing with Firefox",
+        "excerpt":"Is your favorite browser Firefox? If so, you can now share your screen when talking on Daylo Meet from Firefox. It will work even when the other party is using Chrome. No plugins or extensions required! Here is how it works: Make sure you are on Firefox version 62 or...","categories": ["blog"],
+        "tags": ["news","firefox","screen-sharing"],
+        "url": "https://blog.daylo.com/firefox-screen-sharing/",
+        "teaser":null},{
+        "title": "Screen Sharing with Safari",
+        "excerpt":"To share your screen when talking on Daylo Meet from Safari on macOS. You just need to do a few simple configuration changes. Here is how it works! Configuration Make sure you are using the latest version of Safari on macOS or newer. At the time of writing that was...","categories": ["blog"],
+        "tags": ["safari","screen-sharing"],
+        "url": "https://blog.daylo.com/safari-screen-sharing/",
+        "teaser":null},{
+        "title": "Should You Let Your Employees Work From Home",
+        "excerpt":"Most managers will have thought about letting their employees work from home at some point. However, it is never a straightforward decision, as what you gain in one area, you lose in another. While previously, there was a shift towards increasing worker flexibility, the tide seems to have turned back....","categories": ["blog"],
+        "tags": ["remote-work","productivity"],
+        "url": "https://blog.daylo.com/should-you-let-your-employees-work-from-home/",
+        "teaser":"https://blog.daylo.com/assets/images/should-you-let-your-employees-work-from-home/work-from-home.png"},{
+        "title": "Remote Work and Networking: The Virtual Coffee Break",
+        "excerpt":"Remote work is the new normal, and it’s here for the foreseeable future. There are some definite perks to working from home–like not having to actually change out of your pajamas–but there are some difficulties as well. Networking is one of those. How do you network virtually? Enter the virtual...","categories": ["blog"],
+        "tags": ["remote-work","networking"],
+        "url": "https://blog.daylo.com/remote-work-and-networking-the-virtual-coffee-break/",
+        "teaser":"https://blog.daylo.com/assets/images/remote-work-and-networking-the-virtual-coffee-break/kermit-have-a-break.jpg"},{
+        "title": "Vidello Glass",
+        "excerpt":"Quickly sketch out your ideas on a drawing board …   ","categories": ["what-is-new"],
+        "tags": [],
+        "url": "https://blog.daylo.com/what-is-new/002.vidello-glass/item/",
+        "teaser":null},{
+        "title": "Distraction Free Mode",
+        "excerpt":"Give your full attention to your party and the conversation you are having …   ","categories": ["what-is-new"],
+        "tags": [],
+        "url": "https://blog.daylo.com/what-is-new/003.distraction-free-mode/item/",
+        "teaser":null},{
+        "title": "Firefox",
+        "excerpt":"Now you can make video calls using the excellent Mozilla Firefox browser …  ","categories": ["what-is-new"],
+        "tags": [],
+        "url": "https://blog.daylo.com/what-is-new/004.firefox/item/",
+        "teaser":null},{
+        "title": "Safari",
+        "excerpt":"Enjoy Daylo Meet on Safari 11 on macOS High Sierra, macOS Sierra, and macOS El Capitan …  ","categories": ["what-is-new"],
+        "tags": [],
+        "url": "https://blog.daylo.com/what-is-new/005.safari/item/",
+        "teaser":null},{
+        "title": "Distraction Free Mode +",
+        "excerpt":"We replaced the DFM popup with a simple toast notification …   ","categories": ["what-is-new"],
+        "tags": [],
+        "url": "https://blog.daylo.com/what-is-new/006.improved-distraction-free-mode/item/",
+        "teaser":null},{
+        "title": "Edge",
+        "excerpt":"Daylo Meet video calls now work on Microsoft Edge …  ","categories": ["what-is-new"],
+        "tags": [],
+        "url": "https://blog.daylo.com/what-is-new/007.edge/item/",
+        "teaser":null},{
+        "title": "Daylo",
+        "excerpt":"We changed our name from Vidello to Daylo  …  ","categories": ["what-is-new"],
+        "tags": [],
+        "url": "https://blog.daylo.com/what-is-new/008.daylo/item/",
+        "teaser":null},{
+        "title": "Firefox Screen Sharing",
+        "excerpt":"Share your screen from Firefox …  ","categories": ["what-is-new"],
+        "tags": [],
+        "url": "https://blog.daylo.com/what-is-new/009.firefox-screen-sharing/item/",
+        "teaser":null}]
